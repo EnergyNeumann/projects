a6176132d2dd7400fcea548cd3749737cfc54564
Richard <richardklaossneumann@gmail.com>
2023-01-03 14:42:29 -0300

    ta bugado, mas eu centralizei o totced, pelo menos

--- python/desafios/desafio71.py
+++ python/desafios/desafio71.py
@@ -14,12 +14,12 @@ while True:
     else: #se não puder tirar
         if totced > 0:
             print(f'Total de {totced} cédulas de R${ced:.2f}')
-            if ced == 50: #se a cedula for 50 e não der para sacar, muda pra 20
-                ced = 20
-            elif ced == 20: #se a cedula for 20 e não der para sacar, muda pra 10
-                ced = 10
-            elif ced == 10: #se a cedula for 10 e não der para sacar, muda pra 1
-                ced = 1
-            if totalvalor == 0: #se o valor do banco ficar 0, para o programa
-                break
+        if ced == 50: #se a cedula for 50 e não der para sacar, muda pra 20
+            ced = 20
+        elif ced == 20: #se a cedula for 20 e não der para sacar, muda pra 10
+            ced = 10
+        elif ced == 10: #se a cedula for 10 e não der para sacar, muda pra 1
+            ced = 1
+        if totalvalor == 0: #se o valor do banco ficar 0, para o programa
+            break
 print('Volte sempre ao Banco ENE, e tenha um bom dia!')
