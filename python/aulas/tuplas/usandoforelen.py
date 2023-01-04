b0be364322b3f0f83199ad2c8f0d0da2d46c521c
Richard <richardklaossneumann@gmail.com>
2023-01-04 11:02:00 -0300

    usando o for e len para ver a posição e o valor do for

--- python/aulas/tuplas/usandoforelen.py
+++ python/aulas/tuplas/usandoforelen.py
@@ -1,6 +1,9 @@
 lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim') #pode tirar os parenteses, tbm
-print(len(lanche))
+print(len(lanche)) #mostrará 4, pois há 4 valores
 for comida in lanche:
-    print(f'Eu vou comer {comida}')
+    print(f'Eu vou comer {comida}') #pega a comida
 for cont in range(0, len(lanche)): #o cont mostra o primeiro valor, mas nesse caso, por estar pedindo de 0 até o LEN de lanche (é a contagem de valores do lanche), ficaria 0, 1, 2 ,3
-    print(cont)
+    print(cont) #posição
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}') #pega a posição
+for pos, comida in enumerate(lanche):
+    print(f'EU VOU COMER {comida} NA POSIÇÃO {pos}') #pega a posição
