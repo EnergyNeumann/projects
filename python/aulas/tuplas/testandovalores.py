808eb09973860481e91e32e20f20e16d0b74b12c
Richard <richardklaossneumann@gmail.com>
2023-01-04 10:55:47 -0300

    testando os valores da variavel

---
+++ python/aulas/tuplas/testandovalores.py
@@ -0,0 +1,7 @@
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim') #pode tirar os parenteses, tbm
+print(lanche[1])
+print(lanche[2:]) #escreve do 2 em diante
+print(lanche[0:2])
+print(lanche[:2]) #escreve do primeiro até 2 (não mostra 2)
+print(lanche[-3:]) #escreve do ultimo até o primeiro
+#lanche[1] = 'Peixe' #não funciona, pois tuplas são imutáveis
