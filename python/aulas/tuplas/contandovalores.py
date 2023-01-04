58676a4bf4c08417507f135d2be438d4fb3b9ca2
Richard <richardklaossneumann@gmail.com>
2023-01-04 11:15:18 -0300

    posicao da tupla

--- python/aulas/tuplas/contandovalores.py
+++ python/aulas/tuplas/contandovalores.py
@@ -1,4 +1,4 @@
 a = (2, 5, 4)
 b = (5, 8, 1, 2)
 c = a + b
-print(c. count(5)) #vai contar quantas vezes o 5 aparece
+print(c.count(5)) #vai contar quantas vezes o 5 aparece
---
+++ python/aulas/tuplas/posicao.py
@@ -0,0 +1,5 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c.index(4))#mostrar em que posição está tal valor
+print(c.index(5, 1)) #não vai ver a posição 0, começando pela posição 1
