6c7031ca5f2464855576dbb1520a162423aa54b6
Richard <richardklaossneumann@gmail.com>
2023-01-05 16:19:05 -0300

    vendo quantas vogais há na palavra

--- python/desafios/desafio77.py
+++ python/desafios/desafio77.py
@@ -1,7 +1,7 @@
 #Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 palavras = ('JOGAR', 'DIVERTIR', 'EXPLORAR', 'TREINAR', 'ESTUDAR', 'FOCAR')
-for p in palavras:
+for p in palavras: #mostrar as palavras uma dps da outra
     print(F'\nNa palavra {p.upper()} há as vogais:', end =' ')
-    for letra in p:
+    for letra in p: #mostrar as letras
         if letra.lower() in 'aeiou': #se a letra jogada para minuscula for dentro de aeiou, escreve a letra
             print(letra, end='')
