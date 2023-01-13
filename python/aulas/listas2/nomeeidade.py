65cb643f2360ef08b325bcf992a29b321b80f7fa
Richard <richardklaossneumann@gmail.com>
2023-01-13 18:01:17 -0300

    arrumando o nomeeidade, apenas para melhor entendimento, pois ta complicado

--- python/aulas/listas2/nomeeidade.py
+++ python/aulas/listas2/nomeeidade.py
@@ -1,6 +1,11 @@
 galera = [['Richard', 17], ['Richardao', 20], ['Richarda', 15], ['Richardovsk', 40]]
 print(galera[0][0])
+print('*'*20)
+print(galera[1])
+print('*'*20)
 for p in galera:
     print(p)#nome e idade
+    print('*'*20)
     print(p[0])#nome
+    print('*'*20)
     print(p[1])#idade
