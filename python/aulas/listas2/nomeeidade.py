a067c422f502e257f3e6b4f30fe9c96f6d392725
Richard <richardklaossneumann@gmail.com>
2023-01-13 15:54:17 -0300

    for no lista 2, descobrindo idade e nome

---
+++ python/aulas/listas2/nomeeidade.py
@@ -0,0 +1,6 @@
+galera = [['Richard', 17], ['Richardao', 20], ['Richarda', 15], ['Richardovsk', 40]]
+print(galera[0][0])
+for p in galera:
+    print(p)#nome e idade
+    print(p[0])#nome
+    print(p[1])#idade
