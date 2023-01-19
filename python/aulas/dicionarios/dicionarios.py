1a5344d45ccd52a6fafa689642ac17fc2dcad9a7
Richard <richardklaossneumann@gmail.com>
2023-01-19 11:14:40 -0300

    values

--- python/aulas/dicionarios/dicionarios.py
+++ python/aulas/dicionarios/dicionarios.py
@@ -2,3 +2,4 @@ pessoas = {'nome': 'Richard', 'sexo': 'M', 'idade': 22}
 print(pessoas['idade'])
 print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
 print(pessoas.keys())
+print(pessoas.values())
