2e0e79c0bde7bde0b3a31342e4994f16deb0972b
Richard <richardklaossneumann@gmail.com>
2023-01-19 15:49:36 -0300

    desafio92 sobre aposentadoria (calculo), ano de contratação e etc

--- python/desafios/desafio92.py
+++ python/desafios/desafio92.py
@@ -1,3 +1,17 @@
 #Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+from time import sleep
+from datetime import datetime
 pessoa = {}
-pessoa['nome'] = int(input('Nome:'))
+pessoa['nome'] = str(input('Nome: '))
+ano = int(input('Ano de Nascimento: '))
+pessoa['idade'] = datetime.now().year - ano
+pessoa['trabalho'] = int(input('Carteira de Trabalho (0 não tem): '))
+if pessoa['trabalho'] != 0:
+    pessoa['contratação'] = int(input('Ano de Contratação: '))
+    pessoa['salário'] = int(input('Salário: R$'))
+    pessoa['aposentadoria'] = pessoa['idade'] + ((pessoa['contratação'] + 35) - datetime.now().year)
+print('-='*20)
+for k, v in pessoa.items():
+    print(f'{k} é igual a {v}')
+    sleep(1)
+    
