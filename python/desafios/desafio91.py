9b1e46524921e64321c2cb35edfc559bc4ecb3f2
Richard <richardklaossneumann@gmail.com>
2023-01-19 15:28:47 -0300

    adicionando um int antes do randint no desafio91

--- python/desafios/desafio91.py
+++ python/desafios/desafio91.py
@@ -4,7 +4,7 @@ from time import sleep
 from operator import itemgetter
 jogo = {}
 for j in range(1, 5):
-    jogo[f'jogador {j}'] = randint(1, 6)
+    jogo[f'jogador {j}'] = int(randint(1, 6))
 print(jogo)
 ranking = list()
 print('Valores sorteados:')
