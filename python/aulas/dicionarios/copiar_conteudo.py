249e9dc4a44ecb2ddd5f44653431823d414e422a
Richard <richardklaossneumann@gmail.com>
2023-01-19 11:49:35 -0300

    mostrando o conteudo dos dicionarios

--- python/aulas/dicionarios/copiar_conteudo.py
+++ python/aulas/dicionarios/copiar_conteudo.py
@@ -5,4 +5,14 @@ for c in range(0, 3):
     estado['uf'] = str(input('Unidade Federativa: '))
     estado['sigla'] = str(input('Sigla do Estado: '))
     brasil.append(estado.copy())
-print(brasil)
+for e in brasil: #mostrando bonito
+    print(e)
+
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}')
+
+for e in brasil:
+    for v in e.values():
+        print(v, end='') #mostrará os valores (SP, São paulo; RJ, Rio; AC, acre)
+    print()
