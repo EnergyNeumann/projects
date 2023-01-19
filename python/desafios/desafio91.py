2e6a65b8b05d18141d7dbf0d7d58ab3ae9150b81
Richard <richardklaossneumann@gmail.com>
2023-01-19 15:15:20 -0300

    desafio91.py

---
+++ python/desafios/desafio91.py
@@ -0,0 +1,20 @@
+#Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador 1': randint(1, 6),
+        'jogador 2': randint(1, 6),
+        'jogador 3': randint(1, 6),
+        'jogador 4': randint(1, 6)}
+ranking = list()
+print('Valores sorteados:')
+for k, v in jogo.items(): 
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+    ranking = sorted(jogo.items(), key=itemgetter(1), reverse = True) #a chave é o item getter da parte 1 (que são os valores do randint). o reverse é pra deixar do maior para o menor. ISSO VIROU UMA LISTA!!!
+print(ranking)
+print('-='*20)
+print('== RANKING DOS JOGADORES =='.center(10))
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.') #v[0] é o jogador e v[1] é o valor
+    sleep(1)
