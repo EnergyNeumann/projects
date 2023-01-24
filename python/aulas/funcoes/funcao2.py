43afb403c50017bc4e5ae3b261469d599f92b7fa
Richard <richardklaossneumann@gmail.com>
2023-01-24 18:31:43 -0300

    outros jeitos da função

--- python/aulas/funcoes/funcao2.py
+++ python/aulas/funcoes/funcao2.py
@@ -1,6 +1,7 @@
 def soma(a, b):
-    s = a + b
-    print(s)
+    print(f'A = {a} e B = {b}')
+    c = a + b
+    print(f'A soma vale {c}')
 
 # a = 4
 # b = 5
@@ -8,6 +9,8 @@ def soma(a, b):
 # print(c)
 
 soma(4, 5)
+soma(a = 4, b = 5)
+soma(b = 4, a = 5)
 
 # a = 3
 # b = 9
