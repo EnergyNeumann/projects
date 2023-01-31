4ab7e4dd7f74da1d64174b2c1de7f40ca2dd3ce7
Richard <richardklaossneumann@gmail.com>
2023-01-31 13:53:58 -0300

    usando o return

---
+++ python/aulas/funcoes/return.py
@@ -0,0 +1,9 @@
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    # print(f'A soma vale {s}')
+    return s #o s virará o r1, r2 e r3
+
+r1 = somar(2, 3, 5)
+r2 = somar(3, 4)
+r3 = somar(6)
+print(f'Meus calculos deram {r1}, {r2} e {r3}')
