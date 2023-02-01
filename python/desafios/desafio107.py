c3f231fae13662d84fe79397cee0f0c809fbebfa
Richard <richardklaossneumann@gmail.com>
2023-02-01 14:06:15 -0300

    deixando sem formatação de moeda

--- python/desafios/desafio107.py
+++ python/desafios/desafio107.py
@@ -2,6 +2,6 @@
 import desafio107f
 
 num = int(input('Digite o preço: R$'))
-print(f'A metade de R${num:.2f} é R${desafio107f.metade(num):.2f}')
-print(f'O dobro de R${num:.2f} é R${desafio107f.dobro(num):.2f}')
-print(f'Aumentando 10% de R${num:.2f}, temos R${desafio107f.dez(num):.2f}')
+print(f'A metade de R${num:.1f} é R${desafio107f.metade(num):.1f}')
+print(f'O dobro de R${num:.1f} é R${desafio107f.dobro(num):.1f}')
+print(f'Aumentando 10% de R${num:.1f}, temos R${desafio107f.dez(num):.1f}')
