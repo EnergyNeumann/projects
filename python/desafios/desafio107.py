332f4b6034874e05543aeedf16f7dd79ed61f15b
Richard <richardklaossneumann@gmail.com>
2023-02-01 13:58:42 -0300

    colocando titulo do desafio

--- python/desafios/desafio107.py
+++ python/desafios/desafio107.py
@@ -1,3 +1,4 @@
+#Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
 import desafio107f
 
 num = int(input('Digite o preço: R$'))
