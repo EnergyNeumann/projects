ce682a5f317dcfc7d4ec10c887d32263f5149c2b
Richard <richardklaossneumann@gmail.com>
2023-02-09 08:28:38 -0300

    usando o typeof

--- javascript/aulas/constantes/index.js
+++ javascript/aulas/constantes/index.js
@@ -1,2 +1,9 @@
 const nome = 'João';
 console.log(nome);
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+console.log(typeof(primeiroNumero + segundoNumero)) //o resultado da expressão será uma string
