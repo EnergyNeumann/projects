938c7f838748c66b42cd7427859fb6410e5ad5e9
Richard <richardklaossneumann@gmail.com>
2023-02-09 21:21:18 -0300

    mais funções de number

--- javascript/aulas/numeros/index.js
+++ javascript/aulas/numeros/index.js
@@ -3,3 +3,6 @@ let num2 = 2.5;
 num1 = num1.toString()
 console.log(num1.toString() + num2)
 console.log(num1.toFixed(2))//2 casas decimais
+console.log(Number.isInteger(num1))
+let temp = num1 * 'oiii';
+console.log(Number.isNaN(temp))
