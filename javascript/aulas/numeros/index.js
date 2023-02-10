8d00dd1e49396c1cb68c5b6a34527747dafb5a7d
Richard <richardklaossneumann@gmail.com>
2023-02-09 21:07:34 -0300

    função para numbers

---
+++ javascript/aulas/numeros/index.js
@@ -0,0 +1,5 @@
+let num1 = 1;
+let num2 = 2.5;
+num1 = num1.toString()
+console.log(num1.toString() + num2)
+console.log(num1.toFixed(2))//2 casas decimais
--- javascript/aulas/string/index.js
+++ javascript/aulas/string/index.js
@@ -2,3 +2,16 @@ let umaString = "Um texto";
 console.log(umaString.concat(' ', 'em', ' ', 'um'))
 console.log(umaString + ' em um lindo dia ')
 console.log(`${umaString} em um dia lindo`)
+console.log(umaString[4]);
+console.log(umaString.charAt(4)); //pegar elemento da posição 4
+console.log(umaString.indexOf('texto')); //em que posição está texto
+console.log(umaString.indexOf('o', 3))
+console.log(umaString.lastIndexOf('o')) //buscar de trás para frente
+console.log(umaString.replace(/Um/, 'Outra'));
+console.log(umaString.length);
+console.log(umaString.slice(2, 5));
+console.log(umaString.split('r'));//tirar os r
+console.log(umaString.toLowerCase());
+console.log(umaString.toUpperCase());
+
+
