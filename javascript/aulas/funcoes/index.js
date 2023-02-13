3a8ab1f98ac05c5665b460494daf4c057e239030
Richard <richardklaossneumann@gmail.com>
2023-02-13 10:09:07 -0300

    funções parte 2 com arrow string

--- javascript/aulas/funcoes/index.js
+++ javascript/aulas/funcoes/index.js
@@ -2,12 +2,26 @@ function saudacao(nome){
     return `Bom dia ${nome}!`;
 }//não precisa do ;
 
-const variavel = saudacao('Luiz');//chamando a funcao, caso esteja com um console.log(`nome`)
-console.log(variavel) //vai retornar apenas isso, qnd usa o return
-
-function soma(x, y){
+function soma(x = 1, y = 1){//caso n receba valores, serão 1
     const resultado = x + y;
     return resultado
 }
 
+const raiz = function(n){
+    return n ** 0.5;
+}; //precisa do ;, pois ela recebe o valor
+
+const raiz2 = (n) => { //jeito 2 arrow function
+    return n ** 0.5;
+};
+
+const raiz3 = n => n ** 0.5; //jeito 3 
+
+
+
+const variavel = saudacao('Luiz');//chamando a funcao, caso esteja com um console.log(`nome`)
+console.log(variavel) //vai retornar apenas isso, qnd usa o return
+
 console.log(soma(2, 7))
+
+console.log(raiz(16))
