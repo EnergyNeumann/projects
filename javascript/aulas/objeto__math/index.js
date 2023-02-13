221eeca825f7835fefcd8072bf4b0395a8bc2008
Richard <richardklaossneumann@gmail.com>
2023-02-13 08:49:03 -0300

    objeto math

---
+++ javascript/aulas/objeto__math/index.js
@@ -0,0 +1,12 @@
+// let num1 = 9.5443;
+// console.log(num1 ** (1/2))//pega a raiz quadrada do número
+
+// let num2 = Math.floor(num1) //arredonda pra cima
+// let num2 = Math.ceil(num1) //arredonda pra baixo
+// let num2 = Math.round()//arredonda para o mais próximo
+
+// console.log(Math.max(1,2,3,4,5,-10,-1500,2000))//max pega o maior número da sequência. já o min, pega o menor
+
+// const aleatorio = Math.round(Math.random() * (10-5) + 5); //número aleatório entre 10 e 5
+// console.log()//gera número aleatório ente 0 e 1
+
