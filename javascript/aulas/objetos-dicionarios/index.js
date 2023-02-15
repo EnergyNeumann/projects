0e52afb2947db053e8288d234c6c8d26375ed048
Richard <richardklaossneumann@gmail.com>
2023-02-15 08:39:22 -0300

    adaptando a função do objeto

--- javascript/aulas/objetos-dicionarios/index.js
+++ javascript/aulas/objetos-dicionarios/index.js
@@ -4,13 +4,19 @@ const pessoa = {
     idade: 17
 };
 
+// function criaPessoa (nome, sobrenome, idade) {
+//     return {
+//         nome: nome,
+//         sobrenome: sobrenome,
+//         idade: idade
+//     };
+// }
+
 function criaPessoa (nome, sobrenome, idade) {
-    return {
-        nome: nome,
-        sobrenome: sobrenome,
-        idade: idade
-    };
-}
+        return {
+            nome, sobrenome, idade
+        };
+    }
 
 const pessoa1 = criaPessoa('Rich', 'Neu', 17);
 console.log(pessoa1.nome)
