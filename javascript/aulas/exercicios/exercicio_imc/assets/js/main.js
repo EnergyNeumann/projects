c82c978d5f77f15efd1908806fe6e357919a172e
Richard <richardklaossneumann@gmail.com>
2023-02-22 16:42:45 -0300

    colocando os niveis de peso

--- javascript/aulas/exercicios/exercicio_imc/assets/js/main.js
+++ javascript/aulas/exercicios/exercicio_imc/assets/js/main.js
@@ -18,7 +18,40 @@ form.addEventListener('submit', function(e){
         setResultado('Altura inválida', false);
         return;
     }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    console.log(imc);
 });
+
+function getNivelImc (imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9){
+        return nivel[5];
+    }//não precisa de else if, pois o return para a função
+    if(imc >= 34.9){
+        return nivel[4];
+    }
+    if(imc >= 29.9){
+        return nivel[3];
+    }
+    if(imc >= 24.9){
+        return nivel[2];
+    }
+    if(imc >= 18.5){
+        return nivel[1];
+    }
+    if(imc < 18.5){
+        return nivel[0];
+    }
+}
+
+function getImc(peso, altura){
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP(){
     const p = document.createElement('p');
