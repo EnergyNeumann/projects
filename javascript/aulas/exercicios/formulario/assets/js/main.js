b447824c2acda30aa9d3a469c90859aeb8d641a1
Richard <richardklaossneumann@gmail.com>
2023-02-24 08:59:24 -0300

    criando toda estrutura de validação da idade

--- javascript/aulas/exercicios/formulario/assets/js/main.js
+++ javascript/aulas/exercicios/formulario/assets/js/main.js
@@ -13,4 +13,26 @@ form.addEventListener("submit", function(evento){
     }
 });
 
-function 
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, validando){
+    //pegando a área de resultado e colocando em uma variável
+    const resultado = document.querySelector("#resultado");
+
+    resultado.innerHTML = ''; //colocando um inner zerado
+
+    const p = criaP();
+
+    //se for válido, criará a classe resultado-valido. se for inválido, criará a resultado-invalido
+    if (validando){
+        p.classList.add('resultado-valido');
+    }else{
+        p.classList.add('resultado-invalido');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p)
+}
